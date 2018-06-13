b33139c29190bba9824ded5c45a14cdd1793f416
Diego Garcia Lozano <diegogarcialozano95@gmail.com>
2018-06-12 22:52:03 -0300

    Primer comit con el mock de la interfaz

---
+++ src/main/java/ChatFXMLController.java
@@ -0,0 +1,22 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatFXMLController implements Initializable {
+
+    @FXML
+    private Button botonAceptar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    private void clickEnviar(ActionEvent event){
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,54 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public Stage primaryStage;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        mostrarPantallaInicial();
+        primaryStage.show();
+    }
+
+    private void mostrarPantallaInicial() throws IOException {
+        //Le seteo un icono a la app para vender humo
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("img/police.png")));
+
+        //Obtengo un nodo Parent desde el archivo FXML usando un FXMLLoader
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("ChatFXML.fxml"));
+        Parent fileChooserView = loader.load();
+
+        //Creo la escena inicial (y unica escena en este caso) con el nodo Parent creado a partir del FXML
+        Scene initScene = new Scene(fileChooserView);
+
+        //Seteo titulo ni que no se pueda ajustar el tamaño de la ventana
+        primaryStage.setTitle("Privacy Police");
+        primaryStage.setResizable(false);
+
+        //Agrego un EventListener para que si apreta ESC salga de la aplicacion
+        primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, (KeyEvent event) -> {
+            if(KeyCode.ESCAPE == event.getCode()){
+                Platform.exit();
+            }
+        });
+
+        //Seteo la escena cargadad desde el FXML al primary Stage
+        primaryStage.setScene(initScene);
+    }
+}
